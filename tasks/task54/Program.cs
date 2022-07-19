ed060cace4ca810bb1f4c81309cab6561a5cb929
Anna Kerps <anya-kerps@bk.ru>
2022-07-19 21:42:10 +0300

    Задача 54

---
+++ tasks/task54/Program.cs
@@ -0,0 +1,58 @@
+﻿/*Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по 
+убыванию элементы каждой строки двумерного массива.*/
+
+int[,] FillArray(int rowNumber, int colNumber)
+{
+    int[,] result = new int[rowNumber, colNumber];
+    for (int i = 0; i < rowNumber; i++)
+    {
+        for (int j = 0; j < colNumber; j++)
+        {
+            result[i, j] = new Random().Next(1, 100);
+        }
+    }
+    return result;
+}
+
+void ShowArray(int[,] arrayToPrint)
+{
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            Console.Write(arrayToPrint[i, j]);
+            if (j < arrayToPrint.GetLength(1) - 1)
+            {
+                Console.Write("\t");
+            }
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] SortRowArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int buffer = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = buffer;
+                }
+            }
+        }
+    }
+    return array;
+}
+
+int[,] array = FillArray(5, 5);
+ShowArray(array);
+
+Console.WriteLine();
+int[,] sortarray = SortRowArray(array);
+ShowArray(sortarray);
